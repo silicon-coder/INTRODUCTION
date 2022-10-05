96f5eb2c4ffe23f29819d36848fd311ca2843e42
Pawan Pandey <66987283+idevpawan@users.noreply.github.com>
2022-10-05 19:14:37 +0530

    removed logo variable

--- guess_number.py
+++ guess_number.py
@@ -24,7 +24,6 @@ def set_difficulty():
     return HARD_LEVEL_TURNS
 
 def game():
-  print(logo)
   #Choosing a random number between 1 and 100.
   print("Welcome to the Number Guessing Game!")
   print("I'm thinking of a number between 1 and 100.")
